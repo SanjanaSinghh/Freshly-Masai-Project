d1f9a6ed6894006c390e338c11eda2a5dc111531
SanjanaSinghh <sanjana.kolkata13@gmail.com>
2022-04-16 22:41:37 +0530

    final homepage

---
+++ home.js
@@ -0,0 +1,23 @@
+document.querySelector('form').addEventListener('submit',signfrm)
+var usdata;
+if(localStorage.getItem('usdata')===null){
+    usdata = []
+}
+else{
+    usdata = JSON.parse(localStorage.getItem('usdata'));
+}
+function signfrm(){
+    event.preventDefault()
+    var usob = {
+        email: document.querySelector('#mail').value,
+        password: document.querySelector('#password').value,
+    }
+    if(usob.email=='' || usob.password==''){
+   alert('Please fill all the required credentials')
+   return
+    }
+    usdata.push(usob);
+    localStorage.setItem('usdata',JSON.stringify(usdata));
+    alert('User signed in succesfully!')
+     window.location.href = 'plans.html'
+}
